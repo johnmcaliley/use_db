f8037bc11a089ce88a8ae75029a5e0e6b1d35a5b
John McAliley <john@couponshack.com>
2010-12-16 15:05:39 -0500

    change init order

--- lib/use_db/engine.rb
+++ lib/use_db/engine.rb
@@ -3,5 +3,10 @@ require "rails"
 
 module UseDb
   class Engine < Rails::Engine
+    initializer "use_db.plugin" do
+      require File.dirname(__FILE__)+"/use_db_plugin"
+      require File.dirname(__FILE__)+"/migration"
+      ActiveRecord::Base.extend(UseDbPlugin)
+    end
   end
 end
